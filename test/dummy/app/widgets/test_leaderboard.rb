79932faa55712c6bb9551670045d36a20b4b5d23
Stephan Pavlovic <stephan@railslove.com>
2016-01-06 18:24:58 +0100

    Add new widget type - leaderboard

---
+++ test/dummy/app/widgets/test_leaderboard.rb
@@ -0,0 +1,18 @@
+widget :leaderboard do
+  key "x"
+  type "leaderboard"
+  data do
+    {
+      items: [
+        {value: 87809, label: "Step 1"},
+        {value: 70022, label: "Step 2"},
+        {value: 63232, label: "Step 3"},
+        {value: 53232, label: "Step 4"},
+        {value: 32123, label: "Step 5"},
+        {value: 23232, label: "Step 6"},
+        {value: 12232, label: "Step 7"},
+        {value: 2323, label: "Step 8"}
+      ]
+    }
+  end
+end
